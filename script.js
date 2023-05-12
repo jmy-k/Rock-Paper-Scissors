eba10018cc1b8f9738322e5251ef6c7d9c163b79
jmy-k <myjuliakan@gmail.com>
2023-05-11 20:10:48 -0400

    fixed the issue in the previous commit, literally just had to switch gameStatus.textContent with game() function
    added DOM method to print players' choice

--- script.js
+++ script.js
@@ -27,30 +27,34 @@ buttons.forEach((button)=>{
         let rock = "rock";
         let paper="paper";
         let scissors = "scissors";
-        // end game if one of players gets 5 points
-    if (playerScore===5 || computerScore===5){
-        game(playerScore,computerScore);
+    // end game if one of players gets 5 points
+    if (playerScore===5||computerScore===5){
+        return null;
     }
     else{
-            if (element.classList.contains("rock")){
-                console.log (playRound(rock));
-            }
-            else if (element.classList.contains("paper")){
-                console.log(playRound(paper));
-            }
-            else if (element.classList.contains("scissors")){
-                console.log(playRound(scissors));
-            }
-    }
+        if (element.classList.contains("rock")){
+            playRound(rock);
+        }
+        else if (element.classList.contains("paper")){
+            playRound(paper);
+        }
+        else if (element.classList.contains("scissors")){
+            playRound(scissors);
+        }
+    }    
     });
 });
 
 // main game function
-function playRound (playerSelection){
+function playRound(playerSelection){
 
     const computerSelection = getComputerChoice();
-    console.log("computer chooses: " + computerSelection);
-    console.log("player chooses: "+playerSelection);
+
+    // prints players' choices
+    const playerChoice=document.querySelector('#playerchoice');
+    playerChoice.textContent="Player chooses "+playerSelection;
+    const computerChoice=document.querySelector('#computerchoice');
+    computerChoice.textContent="Computer chooses "+computerSelection;
 
     const gameStatus=document.querySelector('.gamestatus');
 
@@ -59,48 +63,48 @@ function playRound (playerSelection){
         const currentPlayerScore = document.querySelector('#playerscore').innerHTML = `Player Score: ${playerScore}`;
         const currentComputerScore = document.querySelector('#computerscore').innerHTML = `Computer Score: ${computerScore}`;
         const currentNumOfGames = document.querySelector('.numofgames').innerHTML = `Number of games played: ${numOfGames}`;
-        game(playerScore,computerScore);
         gameStatus.textContent="Tie! No points added.";
+        game(playerScore,computerScore);
         }
     else if (playerSelection==="rock" && computerSelection==="paper"){
         computerScore++;
         numOfGames++;
         const currentComputerScore = document.querySelector('#computerscore').innerHTML = `Computer Score: ${computerScore}`;
         const currentNumOfGames = document.querySelector('.numofgames').innerHTML = `Number of games played: ${numOfGames}`;
-        game(playerScore,computerScore);
         gameStatus.textContent="You lose! Paper beats rock.";
+        game(playerScore,computerScore);
         }
     else if (playerSelection==="rock" && computerSelection==="scissors"){
         playerScore++;
         numOfGames++;
         const currentPlayerScore = document.querySelector('#playerscore').innerHTML = `Player Score: ${playerScore}`;
         const currentNumOfGames = document.querySelector('.numofgames').innerHTML = `Number of games played: ${numOfGames}`;
-        game(playerScore,computerScore);
         gameStatus.textContent="You win! Rock beats scissors.";
+        game(playerScore,computerScore);
     }
     else if (playerSelection==="paper" && computerSelection==="rock"){
         numOfGames++;
         playerScore++;
         const currentPlayerScore = document.querySelector('#playerscore').innerHTML = `Player Score: ${playerScore}`;
         const currentNumOfGames = document.querySelector('.numofgames').innerHTML = `Number of games played: ${numOfGames}`;
-        game(playerScore,computerScore);
         gameStatus.textContent="You win! Paper beats rock.";
+        game(playerScore,computerScore);
     }
     else if (playerSelection==="paper" && computerSelection==="scissors"){
         numOfGames++;
         computerScore++;
         const currentComputerScore = document.querySelector('#computerscore').innerHTML = `Computer Score: ${computerScore}`;
         const currentNumOfGames = document.querySelector('.numofgames').innerHTML = `Number of games played: ${numOfGames}`;
-        game(playerScore,computerScore);
         gameStatus.textContent="You lose! Scissors beats paper.";
+        game(playerScore,computerScore);
     }
     else if (playerSelection==="scissors" && computerSelection==="paper"){
         numOfGames++;
         playerScore++;
         const currentPlayerScore = document.querySelector('#playerscore').innerHTML = `Player Score: ${playerScore}`;
         const currentNumOfGames = document.querySelector('.numofgames').innerHTML = `Number of games played: ${numOfGames}`;
-        game(playerScore,computerScore);
         gameStatus.textContent="You win! Scissors beats paper.";
+        game(playerScore,computerScore);
         
     }
     else if (playerSelection==="scissors" && computerSelection==="rock"){
@@ -108,8 +112,8 @@ function playRound (playerSelection){
         computerScore++;
         const currentComputerScore = document.querySelector('#computerscore').innerHTML = `Computer Score: ${computerScore}`;
         const currentNumOfGames = document.querySelector('.numofgames').innerHTML = `Number of games played: ${numOfGames}`;
-        game(playerScore,computerScore);
         gameStatus.textContent="You lose! Rock beats scissors.";
+        game(playerScore,computerScore);
     }
 }
     
@@ -117,24 +121,23 @@ function playRound (playerSelection){
 // score track
 function game(playerScore,computerScore){
     if (playerScore===5 || computerScore===5){
+        const finalGameStatus = document.createElement('div');
+        const gameStatus=document.querySelector('.gamestatus');
         if (playerScore > computerScore){
-            const gameStatus = document.querySelector('.gamestatus').innerHTML = `Game over! Player Wins!`;
+            finalGameStatus.textContent = `Game over! Player Wins!`;
+            gameStatus.appendChild(finalGameStatus);
         }
         else if (computerScore > playerScore){
-            const gameStatus = document.querySelector('.gamestatus').innerHTML = `Game over! Computer Wins!`;
+            finalGameStatus.textContent = `Game over! Computer Wins!`;
+            gameStatus.appendChild(finalGameStatus);
         }
         else{
-            const gameStatus = document.querySelector('.gamestatus').innerHTML = `Game over! Tie!`;
+            finalGameStatus.textContent = `Game over! Tie!`;
+            gameStatus.appendChild(finalGameStatus);
         }
     }
 }
 
 
-//function game(){
-//    for (let i = 1; i <= 5; i++){
-//        console.log(i);
-//        console.log(playRound());
-//        }
-
-
-// console.log(game())
+// put the button event listeners inside the game function
+// 
